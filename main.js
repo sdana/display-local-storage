4eea317c409702ad5a757762f05a1b87c109781c
sdana <seth.dana@gmail.com>
2018-06-04 15:02:33 -0500

    added card styles in main.css

--- main.js
+++ main.js
@@ -3,20 +3,29 @@ const inventoryDatabase = JSON.parse(homeInventory)
 const articleElement = document.querySelector("#my-stuff")
 
 
-const addDomElement = (currentElement) => {
+const addDomElement = (currentElement, desiredTable) => {
     
     const sectionEl = document.createElement("section")
     const keyArray = ["name","location","description"]
 
-    for (let i=0; i<keyArray.length; i++) {
-        const pEl = document.createElement("p")
-        let objectKey = keyArray[i]
-        console.log("objectKey", objectKey)
-        console.log("element", currentElement[objectKey])
-        pEl.innerHTML = currentElement[objectKey]
-        sectionEl.appendChild(pEl)
-        articleElement.appendChild(sectionEl)
-    }  
+    if (desiredTable === undefined){
+        for (let i=0; i<keyArray.length; i++) {
+            const pEl = document.createElement("p")
+            let objectKey = keyArray[i]
+            pEl.innerHTML = currentElement[objectKey]
+            sectionEl.appendChild(pEl)
+            articleElement.appendChild(sectionEl)
+        }
+    }
+    else {
+        for (let i=0; i<keyArray.length; i++) {
+            const pEl = document.createElement("p")
+            let objectKey = keyArray[i]
+            pEl.innerHTML = currentElement[objectKey]
+            sectionEl.appendChild(pEl)
+            articleElement.appendChild(sectionEl)
+        }
+    }
 }
 
 for (let items in inventoryDatabase) {
