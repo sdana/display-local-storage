3b723bbcdc924b6bd78ffa288b8d0ac31aef622d
sdana <seth.dana@gmail.com>
2018-06-04 15:14:20 -0500

    added function in order to select which list of items you want inside the homeInventory object

--- main.js
+++ main.js
@@ -3,35 +3,25 @@ const inventoryDatabase = JSON.parse(homeInventory)
 const articleElement = document.querySelector("#my-stuff")
 
 
-const addDomElement = (currentElement, desiredTable) => {
+const addDomElement = (currentElement) => {
     
     const sectionEl = document.createElement("section")
     const keyArray = ["name","location","description"]
 
-    if (desiredTable === undefined){
-        for (let i=0; i<keyArray.length; i++) {
-            const pEl = document.createElement("p")
-            let objectKey = keyArray[i]
-            pEl.innerHTML = currentElement[objectKey]
-            sectionEl.appendChild(pEl)
-            articleElement.appendChild(sectionEl)
-        }
-    }
-    else {
-        for (let i=0; i<keyArray.length; i++) {
-            const pEl = document.createElement("p")
-            let objectKey = keyArray[i]
-            pEl.innerHTML = currentElement[objectKey]
-            sectionEl.appendChild(pEl)
-            articleElement.appendChild(sectionEl)
-        }
+    for (let i=0; i<keyArray.length; i++) {
+        const pEl = document.createElement("p")
+        let objectKey = keyArray[i]
+        pEl.innerHTML = currentElement[objectKey]
+        sectionEl.appendChild(pEl)
+        articleElement.appendChild(sectionEl)
     }
 }
 
-for (let items in inventoryDatabase) {
-    // console.log(items)
-    for (let i=0; i<inventoryDatabase[items].length; i++){
+const listIterator = (listDesired) => {
+    for (let i=0; i<listDesired.length; i++){
         // console.log(inventoryDatabase[items][i])
-        addDomElement(inventoryDatabase[items][i])
+        addDomElement(listDesired[i])
     }
 }
+
+listIterator(inventoryDatabase["furniture"])
